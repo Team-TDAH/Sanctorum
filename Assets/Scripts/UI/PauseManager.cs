1ef598b9160cbbd4e8c76ab177f700ca96e98e4d
DiegoPereyra21 <diegoromanpereyra2102@gmail.com>
2026-07-03 01:22:49 -0300

    Add a “simple” pause menu
    Agregue el menu de pausa in game que la idea era que sea muy simple, pero por temas de las habilidad y el dash, termine agrandandolo bastante, pero no tiene bugs como disparar o gastar el dash en medio de la pausa.

---
+++ Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private PlayerInput playerInput;
+    private InputAction pauseAction;
+    private bool isPaused;
+    [SerializeField] private GameObject settingsPanel;
+
+    //necesario porque al pausar me estaba creando orbes de luz al apretar las opciones del menu de pausa
+    //no me gusto para nada la solucion, pero es la unica forma de que el dash no se consuma mientras este en la pausa y aprete dash
+    [SerializeField] private AbilityManager abilityManager;
+    [SerializeField] private PlayerController playerController;
+ 
+ 
+    private void Awake()
+    {
+        if (playerInput != null)
+            //no es ideal ponerlo asi, pero no quiero mas referencias para un "simple" script de pausa
+            pauseAction = playerInput.actions["Pause"];
+    }
+    private void OnEnable()
+    {
+        if (pauseAction != null) pauseAction.Enable();
+    }
+    private void OnDisable()
+    {
+        if (pauseAction != null) pauseAction.Disable();
+    }
+    private void Update()
+    {
+        if (pauseAction != null && pauseAction.WasPressedThisFrame())
+            TogglePause();
+    }
+    private void TogglePause()
+    {
+        isPaused = !isPaused;
+        pausePanel.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0f : 1f;
+        abilityManager.InputEnabled = !isPaused;
+        playerController.InputEnabled = !isPaused; 
+    }
+
+    
+    public void Resume()
+    {
+        TogglePause();
+    }
+
+        public void OnSettingsPressed()
+    {
+        pausePanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+        public void OnSettingsBackPressed()
+    {
+        pausePanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+        public void BackToMainMenu()
+    {
+        TogglePause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
+
+
+//esta era la idea pero recorde que no tengo el sistema de inputs viejo, no pense que el input actual seria tan molesto
+/*
+    [SerializeField] private GameObject pausePanel;
+    //me di cuent aahora, que no estaba siguiendo la arquitectura camelCase en las variables,lpm
+    [SerializeField] private GameObject settingsPanel;
+    private bool isPaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            TogglePause();
+    }
+    private void TogglePause()
+    {
+        isPaused = !isPaused;
+        pausePanel.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0f : 1f;
+    }
+    public void Resume()
+    {
+        TogglePause();
+    }
+    */
