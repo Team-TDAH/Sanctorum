f26fb767ad50148f64b251aa1c0312a05288a260
DiegoPereyra21 <diegoromanpereyra2102@gmail.com>
2026-07-08 21:37:44 -0300

    fix

--- Assets/Scripts/Enemies/Boss1/Ferryman/BossFerryman.cs
+++ Assets/Scripts/Enemies/Boss1/Ferryman/BossFerryman.cs
@@ -2,51 +2,43 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-/// <summary>
-/// El jefe derrotado convertido en barquero: al interactuar lleva a la siguiente escena.
-/// GameObject con Collider2D en modo trigger, empieza desactivado.
-/// BossRegister lo activa cuando el jefe muere o cuando ya estaba muerto al cargar.
-/// </summary>
+//bossRegister hace desaparecer al boss luego de derrotarlo y activar al ferryman para que nos pueda llevar a la siguiente escena o luego volver
 public class BossFerryman : MonoBehaviour
 {
-    //nombre exacto de la escena a cargar, tiene que estar en el build profiles
+    //para que luego no me olvide donde esta el conetenedor de la escena a la que quiero ir (tambien habra 2 ferryman, uno que te lleve a la siguiente y otro que te traiga a la anterior)
     [SerializeField] private string nextSceneName;
-    //cartel de "presiona E" o similar, opcional, hijo de este gameobject
+    //el gameobject que debe tener algun mensaje como "E" o "Press E" avisando que se puede interactuar con el mismo, ya luego agregare los dialogos y demas
     [SerializeField] private GameObject interactPrompt;
-
     private InputAction interactAction;
+    //para saber cuando mostrar el mensaje y cuando no, el de la E
     private bool playerInRange;
-
-
     private void Awake()
     {
-        //mismo patron que el pausemanager, el player es de otro prefab
         var playerInput = FindAnyObjectByType<PlayerInput>();
         if (playerInput != null)
+        {
+            //en esete caso no tengo que crear un nuevo input, existe uno llamado interact
             interactAction = playerInput.actions["Interact"];
+        }
     }
-
     private void OnEnable()
     {
         if (interactAction != null) interactAction.Enable();
     }
-
     private void OnDisable()
     {
         if (interactAction != null) interactAction.Disable();
     }
-
     private void Update()
     {
         if (!playerInRange) return;
 
         if (interactAction != null && interactAction.WasPressedThisFrame())
         {
-            //aca ira la animacion de transicion del barco cuando la tengas
+            //ANIMACION DE TRANSICION ACA!!!!!!!!!!!!!!!!!
             SceneManager.LoadScene(nextSceneName);
         }
     }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<PlayerController>() == null) return;
@@ -54,7 +46,6 @@ public class BossFerryman : MonoBehaviour
         playerInRange = true;
         if (interactPrompt != null) interactPrompt.SetActive(true);
     }
-
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.GetComponent<PlayerController>() == null) return;
