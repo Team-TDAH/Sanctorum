aff29b19005c8a3b9d4da7a29aa938dd9b563561
DiegoPereyra21 <diegoromanpereyra2102@gmail.com>
2026-09-25 16:28:45 -0300

    F1.5: IDamageable en su propio archivo

--- Assets/_Project/Scripts/Abilities/Definitions/LightOrb/LightOrbProjectile.cs
+++ Assets/_Project/Scripts/Abilities/Definitions/LightOrb/LightOrbProjectile.cs
@@ -68,9 +68,3 @@ public class LightOrbProjectile : MonoBehaviour
         Destroy(gameObject);
     }
 }
-
-//interfaz que cualquier entidad dañable debe implementar para no acoplarse
-public interface IDamageable
-{
-    void TakeDamage(int amount);
-}
---
+++ Assets/_Project/Scripts/Combat/IDamageable.cs
@@ -0,0 +1,5 @@
+//interfaz que cualquier entidad dañable debe implementar para no acoplarse
+public interface IDamageable
+{
+    void TakeDamage(int amount);
+}
